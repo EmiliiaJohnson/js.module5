a5694b891a95b1ab205cd6dc6dfffc719b1afffa
Emiliia Johnson <117929541+EmiliiaJohnson@users.noreply.github.com>
2022-12-08 18:46:22 +0400

    Task correction

--- 5.js
+++ 5.js
@@ -1,6 +1,6 @@
 let array = [3, 14, 15, 92, 6];
-let result = array.map(function(array) {
-    return array;
-});
-console.log(array.length);
-console.log(result);
+console.log("array length = "+ array.length);
+
+for(const number of array) {
+  console.log(number);
+}
